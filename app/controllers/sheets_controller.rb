47fce56a705b5079e66cfa3541288b8a3d71daf4
rochefort <teraswan@gmail.com>
2012-01-31 01:56:59 +0900

    delete comment

--- app/controllers/sheets_controller.rb
+++ app/controllers/sheets_controller.rb
@@ -1,8 +1,8 @@
 class SheetsController < ApplicationController
   def index
-    @sheet = Sheet.find(1) # TODO: userと絡める　& 諸々sheet controllerへ移動
-    @groups = {}
+    @sheet = Sheet.find(1) # TODO: userと絡める
     # column_no毎にグループ化
+    @groups = {}
     groups = Group.order('groups.ordinal, key_mappings.ordinal').includes(:key_mappings)
     groups.chunk{|group| group.column_no }.each{ |k,v| @groups[k] = v }
   end
